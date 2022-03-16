07b6615f8cb5ce13c0e7321d253a708047e76eb5
Prakash Gurung <grgmo88@gmail.com>
2022-03-16 10:09:09 +0000

    add pre-commit hook to check lint, type and unit tests

--- src/App.tsx
+++ src/App.tsx
@@ -70,9 +70,7 @@ const App = () => {
         style={backgroundStyle}>
         <Header />
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+          style={{backgroundColor: isDarkMode ? Colors.black : Colors.white}}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
